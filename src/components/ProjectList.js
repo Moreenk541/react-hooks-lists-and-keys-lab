810207546180aa6fc498769472e0b33752d5b1b7
moreen karimi <moreenk541@gmail.com>
2025-04-15 16:42:36 +0300

    used map

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
@@ -6,7 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
